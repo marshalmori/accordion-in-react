0b156c28ce7dffade3a6e08de7ddf6d117a1ac08
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-08-24 17:44:44 -0300

    Toggle accordion

--- src/components/AccordionItem.jsx
+++ src/components/AccordionItem.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import { useState } from "react";
 
 const AccordionItem = ({ num, title, text }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="item">
+    <div className="item" onClick={handleToggle}>
       <p className="number">{num > 9 ? `0${num + 1}` : num + 1}</p>
       <p className="title">{title}</p>
-      <p className="icon">-</p>
-      <div className="content-box">{text}</div>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen && <div className="content-box">{text}</div>}
     </div>
   );
 };
